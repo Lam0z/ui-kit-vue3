9474914c2f0ee8e0c7787351f16e97abd521a5e7
Nick Efimov <lamoz760@gmail.com>
2023-12-30 16:00:56 +0300

    Change base

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
     plugins: [vue()],
     resolve: {
-        base: "/ui-kit/",
+        base: "/ui-kit-vue3/",
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
