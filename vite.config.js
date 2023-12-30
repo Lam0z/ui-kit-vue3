cff572a1355128cfe9a602bdd301e4ac5360d571
Nick Efimov <lamoz760@gmail.com>
2023-12-30 16:19:04 +0300

    change

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    base: "/<repo>/",
+    base: "/ui-kit-vue3/",
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
